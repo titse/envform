9a764726a0336e8d84e900212104856698124d17
Tiffany Tse <ttse@stainlesscode.com>
2019-02-04 12:21:21 -0800

    init changes

--- EnvironmentalHealthForm/Form1.cs
+++ EnvironmentalHealthForm/Form1.cs
@@ -16,5 +16,10 @@ namespace EnvironmentalHealthForm
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
